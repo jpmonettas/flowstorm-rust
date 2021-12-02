3789073944bf117ae1f9814bf1ade667da13b0a3
filip <fbielejec@gmail.com>
2021-12-02 14:23:29 +0100

    commit changes

--- src/app.rs
+++ src/app.rs
@@ -1,34 +1,32 @@
-use {egui, epi};
+use std::sync::mpsc::Sender;
 use std::sync::{Arc, Mutex};
-use std::sync::mpsc;
-use std::sync::mpsc::{Sender, Receiver};
 
 #[derive(Debug)]
 pub struct DebuggerState {
     pub value: f32,
-	
 }
 
 impl Default for DebuggerState {
     fn default() -> Self {
-		Self {            
-			value: 2.7,
-		}		
-	}
+        Self { value: 2.7 }
+    }
 }
 
 pub struct DebuggerApp {
-	state_arc: Arc<Mutex<DebuggerState>>,
-	pub ctx_chan_sender: Sender<egui::CtxRef>,
+    state_arc: Arc<Mutex<DebuggerState>>,
+    pub ctx_chan_sender: Sender<egui::CtxRef>,
 }
 
 impl DebuggerApp {
-	pub fn new(state_arc: Arc<Mutex<DebuggerState>>, ctx_chan_sender: Sender<egui::CtxRef>) -> Self {
-		Self {
-			state_arc,
+    pub fn new(
+        state_arc: Arc<Mutex<DebuggerState>>,
+        ctx_chan_sender: Sender<egui::CtxRef>,
+    ) -> Self {
+        Self {
+            state_arc,
             ctx_chan_sender,
-		}
-	}
+        }
+    }
 }
 
 impl epi::App for DebuggerApp {
@@ -43,21 +41,24 @@ impl epi::App for DebuggerApp {
         _frame: &mut epi::Frame<'_>,
         _storage: Option<&dyn epi::Storage>,
     ) {
-        self.ctx_chan_sender.send(egui::CtxRef::clone(_ctx));        
+        self.ctx_chan_sender
+            .send(_ctx.clone())
+            .expect("Could not send to context channel during setup");
     }
 
-   
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-    fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
-		println!("REPAINTING..................");
-        let Self { state_arc, ctx_chan_sender: _ } = self;
-		let mut state = state_arc.lock().unwrap();
-		
+    fn update(&mut self, ctx: &egui::CtxRef, _frame: &mut epi::Frame<'_>) {
+        println!("REPAINTING..................");
+
+        let mut state = self
+            .state_arc
+            .lock()
+            .expect("Could not obtain a lock on state");
+
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Flowstorm debugger");
-			ui.add(egui::Slider::new(&mut state.value, 0.0..=10.0).text("value"));
+            ui.add(egui::Slider::new(&mut state.value, 0.0..=10.0).text("value"));
         });
-        
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,43 @@
 #![forbid(unsafe_code)]
 #![cfg_attr(not(debug_assertions), deny(warnings))] // Forbid warnings in release builds
-#![warn(clippy::all, rust_2018_idioms)]
+#![warn(rust_2018_idioms)]
 
+use epi::NativeOptions;
+use std::sync::mpsc;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
-use egui::CtxRef;
-use std::sync::mpsc;
-use {egui, epi};
-use epi::NativeOptions;
-use egui_glium;
 mod app;
 
 fn main() {
     let debugger_state_arc = Arc::new(Mutex::new(app::DebuggerState::default()));
 
-	let thread_state_ref = Arc::clone(&debugger_state_arc);
+    let thread_state_ref = Arc::clone(&debugger_state_arc);
     let (tx, rx) = mpsc::channel();
-	
-	let dsa = app::DebuggerApp::new(Arc::clone(&debugger_state_arc), tx.clone());
-    
-	let _h = thread::spawn (move || {
-		let ctx = rx.recv().unwrap();
-		loop {            
-			{ // this block is so we release the thread_ref mutex loc on evenry iteration
-				// after aquiring it, since the UI is using the same lock on repaint	
-				let mut state = thread_state_ref.lock().unwrap();
-				println!("*******{:?}", state);
-				state.value += 0.1;
-				println!("Painting with CTX");                
-			}
-			ctx.request_repaint();
+
+    let dsa = app::DebuggerApp::new(Arc::clone(&debugger_state_arc), tx);
+
+    let _h = thread::spawn(move || {
+        let ctx = rx.recv().unwrap();
+        loop {
+            let mut state = thread_state_ref.lock().unwrap();
+            println!("*******{:?}", state);
+            state.value += 0.1;
+            // drop mutex guard explicitely
+            // so we release the thread_ref mutex loc on every iteration
+            // after aquiring it, since the UI is using the same lock on repaint
+            drop(state);
+
+            println!("Painting with CTX");
+
+            ctx.request_repaint();
             thread::sleep(Duration::from_millis(1000));
-		}
-		
-	});
-	
+        }
+    });
+
     // h.join().unwrap();
-    
+
     let native_options = NativeOptions::default();
-	egui_glium::run(Box::new(dsa), &native_options)
-	//eframe::run_native(Box::new(dsa), native_options);
-    
+    egui_glium::run(Box::new(dsa), &native_options)
+    //eframe::run_native(Box::new(dsa), native_options);
 }
