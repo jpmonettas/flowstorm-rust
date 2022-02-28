d50f7e21d0ca6393267114b86fb826f0e0852f12
Juan Monetta <jpmonettas@gmail.com>
2022-02-28 14:45:44 -0300

    Add CallStackTree type

--- src/util_types.rs
+++ src/util_types.rs
@@ -1,5 +1,6 @@
-use crate::state::{Form, FormId};
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use crate::state::{Form, FormId,ExecTrace, FnCallTrace};
 
 #[derive(Debug)]
 pub struct SortedForms {
@@ -50,23 +51,6 @@ impl SortedForms {
                 }
             }
         }
-        // if !self.forms_map.contains_key(&form_id) {
-        //     let timestamp = form.timestamp.clone();
-        //     self.forms_map.insert(form_id, form);
-        //     let idx_result = self
-        //         .order
-        //         .as_slice()
-        //         .binary_search_by(|(ts, _)| ts.cmp(&timestamp).reverse());
-
-        //     match idx_result {
-        //         Ok(idx) => {
-        //             self.order.insert(idx, (timestamp, form_id));
-        //         }
-        //         Err(idx) => {
-        //             self.order.insert(idx, (timestamp, form_id));
-        //         }
-        //     }
-        // }
     }
 
 	#[allow(dead_code)]
@@ -92,12 +76,75 @@ impl<'a> Iterator for SortedFormsIter<'a> {
     }
 }
 
+#[derive(Debug)]
+struct CallStackTreeNode<'a> {
+	pub fn_call: &'a FnCallTrace,
+	pub childs: Vec<Arc<Mutex<CallStackTreeNode<'a>>>>
+}
+
+#[derive(Debug)]
+pub struct CallStackTree<'a> {
+	root: Arc<Mutex<CallStackTreeNode<'a>>>,
+	stack: Vec<Arc<Mutex<CallStackTreeNode<'a>>>>	
+}
+
+impl<'a> CallStackTree<'a> {
+	pub fn new(root_e: &'a FnCallTrace) -> Self {        
+		let mut tree = Self {
+			root: Arc::new(Mutex::new(CallStackTreeNode {
+				fn_call: root_e,
+				childs: vec![]
+			})),
+			stack: vec![]
+		};
+        
+		tree.stack.push(Arc::clone(&tree.root));
+        tree
+	}
+
+	fn call(&mut self, e: &'a FnCallTrace) {
+        
+		let node_pointer = Arc::new(Mutex::new(CallStackTreeNode {
+			fn_call: e,
+			childs: vec![]
+		}));
+        
+		let stack_node_pointer = Arc::clone(&node_pointer);
+
+		// add the node to current function childs
+		{
+			let mut curr_func_pointer = self.stack.as_slice().last().expect("Stack shouldn't be empty").lock().unwrap();
+			curr_func_pointer.childs.push(node_pointer);
+		}
+        
+		self.stack.push(stack_node_pointer);
+       
+	}
+
+	fn pop(&mut self) {
+        self.stack.pop();
+	}
+
+	pub fn add_trace(&mut self, trace: &'a ExecTrace) {
+		match trace {
+			ExecTrace::FnCallTrace(fct) => self.call(fct),
+			
+			ExecTrace::ExprTrace(et) => if et.is_outer_form {
+				self.pop();
+			},
+		};
+	}
+	
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
-
+	use crate::state::{ExprTrace};
+	
     #[test]
-    fn basic_test() {
+    fn sorted_forms_basic_test() {
         let mut sfs = SortedForms::new();
 
         sfs.insert(1, Form::new(1, "testns".to_string(), "Form1".to_string(), 50));
@@ -113,4 +160,39 @@ mod tests {
         let rv = sfs.iter().map(|f| f.timestamp).collect::<Vec<u64>>();
         assert_eq!(rv, vec![70, 50, 20, 11, 10]);
     }
+
+    #[test]
+	fn callstack_tree_test() {
+		let first_trace = FnCallTrace {form_id: 0, fn_name: "fn1".to_string(), fn_ns: "ns1".to_string(), args_vec: "[]".to_string(), timestamp: 1};
+
+		let rest_traces = vec![
+            
+			ExecTrace::FnCallTrace(FnCallTrace {form_id: 0, fn_name: "fn1-1".to_string(), fn_ns: "ns1".to_string(), args_vec: "[]".to_string(), timestamp: 1}),
+			ExecTrace::ExprTrace(ExprTrace{form_id: 0, result:"R1-1".to_string(), coord: vec![], timestamp: 1, is_outer_form: true}),
+			
+			ExecTrace::FnCallTrace(FnCallTrace {form_id: 0, fn_name: "fn1-2".to_string(), fn_ns: "ns1".to_string(), args_vec: "[]".to_string(), timestamp: 1}),
+			ExecTrace::ExprTrace(ExprTrace{form_id: 0, result:"R1-2".to_string(), coord: vec![], timestamp: 1, is_outer_form: true}),
+
+			ExecTrace::FnCallTrace(FnCallTrace {form_id: 0, fn_name: "fn1-3".to_string(), fn_ns: "ns1".to_string(), args_vec: "[]".to_string(), timestamp: 1}),
+			ExecTrace::FnCallTrace(FnCallTrace {form_id: 0, fn_name: "fn1-3-1".to_string(), fn_ns: "ns1".to_string(), args_vec: "[]".to_string(), timestamp: 1}),
+			ExecTrace::ExprTrace(ExprTrace{form_id: 0, result:"R1-3-1".to_string(), coord: vec![], timestamp: 1, is_outer_form: true}),
+			ExecTrace::ExprTrace(ExprTrace{form_id: 0, result:"R1-3".to_string(), coord: vec![], timestamp: 1, is_outer_form: true}),
+			
+			ExecTrace::ExprTrace(ExprTrace{form_id: 0, result:"R1".to_string(), coord: vec![], timestamp: 1, is_outer_form: true}),
+		];
+
+        
+		let mut callstack_tree: CallStackTree = CallStackTree::new(&first_trace);
+
+        for t in &rest_traces {
+			callstack_tree.add_trace(t);
+		}
+		
+		// Just checking the structure by eye
+		// TODO: make a proper test
+		println!("{:#?}", &callstack_tree);        
+		assert!(true);
+		
+		
+	}
 }
